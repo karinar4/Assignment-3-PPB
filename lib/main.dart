b90e98909bb1448d0edc9b36d6c69978211c1e34
karinar4 <karinarahmawati219@gmail.com>
2025-05-20 12:41:45 +0700

    fix: init notification

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,14 @@ import 'package:firebase_autentikasi/screens/register.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_autentikasi/services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await NotificationService.initializeNotification();
   runApp(const MyApp());
 }
 
